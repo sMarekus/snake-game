8631f99ceae039d55d9d2479d5c3b48859351c89
Marekus <marek.stransky1@gmail.com>
2023-06-24 11:49:55 +0200

    Adding high score

--- main.py
+++ main.py
@@ -37,13 +37,13 @@ while game_is_on:
 
     #Detect collision with the wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
     #Detect collision with the tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
 
 screen.exitonclick()
--- score.py
+++ score.py
@@ -8,14 +8,23 @@ class Score(Turtle):
         self.penup()
         self.color("white")
         self.score = 0
+        self.high_score = 0
         self.goto(x=0, y=260)
-        self.write(f"Home = {self.score}", font=FONT_STYLE, align="center")
+        self.update_score()
         self.hideturtle()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", font=FONT_STYLE, align="center")
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
+
+    # If you want to end the game
+    #
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", font=FONT_STYLE, align="center")
 
     def update_score(self):
         self.clear()
-        self.write(f"Home = {self.score}", font=FONT_STYLE, align="center")
+        self.write(f"Score: {self.score} | High Score: {self.high_score}", font=FONT_STYLE, align="center")
